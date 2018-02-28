5a2e3b637f5ac1c39b3d4f280f3a679aec67206c
eoconnor6797 <eoconnor6797@yahoo.com>
2018-02-27 20:03:34 -0500

    stuff mostly works

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # TaskTracker
-* Users can register and log to the task tracker
-* Logged in users will see a link from the login page to the in progress tasks, completed tasks, and a form to make new tasks
-* Users can assign tasks to other users
-* Users can log work on tasked that are assigned to them and mark them as complete
-* Users can reassign tasks that are assigned to them
-* Completed tasks cannot be modified
+  * Users can register and log to the task tracker
+  * When a User is registering, they can select another user as their manager
+    - this cannot change once it is set
+  * Logged in users will see a link from the login page to the in progress tasks, completed tasks, 
+  a form to make new tasks, a link to their profile and a link to their lackey's status
+  * The users that are managed by another user are known as that user's "lackeys"
+  * On a User's profile, they can see their manager as well as their lackeys
+  * Users can assign tasks to anyone that they manage
+  * If a user does not have a manager they cannot be assigned tasks
+  * If a user has not lackeys they cannot assign tasks
+  * Users can log work on tasked that are assigned to them and mark them as complete
+  * Users can reassign tasks that are assigned to one of their lackeys
+  * Users can no log work in real time on their tasks.
+    - While on the 'details' page of a task, the assigned user can
+      'begin work' and then 'end work' to make a duration of time worked
+      on a task.
+  * Completed tasks cannot be modified
+  * Time durations cannot be edited
 
 To start your Phoenix server:
 
--- test/task_tracker/tracker/tracker_test.exs
+++ test/task_tracker/tracker/tracker_test.exs
@@ -68,4 +68,66 @@ defmodule TaskTracker.TrackerTest do
       assert %Ecto.Changeset{} = Tracker.change_task(task)
     end
   end
+
+  describe "timeblocks" do
+    alias TaskTracker.Tracker.Time_block
+
+    @valid_attrs %{end_time: "2010-04-17 14:00:00.000000Z", start_time: "2010-04-17 14:00:00.000000Z"}
+    @update_attrs %{end_time: "2011-05-18 15:01:01.000000Z", start_time: "2011-05-18 15:01:01.000000Z"}
+    @invalid_attrs %{end_time: nil, start_time: nil}
+
+    def time_block_fixture(attrs \\ %{}) do
+      {:ok, time_block} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Tracker.create_time_block()
+
+      time_block
+    end
+
+    test "list_timeblocks/0 returns all timeblocks" do
+      time_block = time_block_fixture()
+      assert Tracker.list_timeblocks() == [time_block]
+    end
+
+    test "get_time_block!/1 returns the time_block with given id" do
+      time_block = time_block_fixture()
+      assert Tracker.get_time_block!(time_block.id) == time_block
+    end
+
+    test "create_time_block/1 with valid data creates a time_block" do
+      assert {:ok, %Time_block{} = time_block} = Tracker.create_time_block(@valid_attrs)
+      assert time_block.end_time == DateTime.from_naive!(~N[2010-04-17 14:00:00.000000Z], "Etc/UTC")
+      assert time_block.start_time == DateTime.from_naive!(~N[2010-04-17 14:00:00.000000Z], "Etc/UTC")
+    end
+
+    test "create_time_block/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Tracker.create_time_block(@invalid_attrs)
+    end
+
+    test "update_time_block/2 with valid data updates the time_block" do
+      time_block = time_block_fixture()
+      assert {:ok, time_block} = Tracker.update_time_block(time_block, @update_attrs)
+      assert %Time_block{} = time_block
+      assert time_block.end_time == DateTime.from_naive!(~N[2011-05-18 15:01:01.000000Z], "Etc/UTC")
+      assert time_block.start_time == DateTime.from_naive!(~N[2011-05-18 15:01:01.000000Z], "Etc/UTC")
+    end
+
+    test "update_time_block/2 with invalid data returns error changeset" do
+      time_block = time_block_fixture()
+      assert {:error, %Ecto.Changeset{}} = Tracker.update_time_block(time_block, @invalid_attrs)
+      assert time_block == Tracker.get_time_block!(time_block.id)
+    end
+
+    test "delete_time_block/1 deletes the time_block" do
+      time_block = time_block_fixture()
+      assert {:ok, %Time_block{}} = Tracker.delete_time_block(time_block)
+      assert_raise Ecto.NoResultsError, fn -> Tracker.get_time_block!(time_block.id) end
+    end
+
+    test "change_time_block/1 returns a time_block changeset" do
+      time_block = time_block_fixture()
+      assert %Ecto.Changeset{} = Tracker.change_time_block(time_block)
+    end
+  end
 end
